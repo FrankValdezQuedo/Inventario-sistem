dca47a2ccf6014944dfa8dba77922234f7ceefee
FrankValdezQuedo <109559378+FrankValdezQuedo@users.noreply.github.com>
2025-01-09 18:15:28 -0500

    primer commit

---
+++ src/main/java/com/flcode/inventario_sistem/InventarioSistemApplication.java
@@ -0,0 +1,13 @@
+package com.flcode.inventario_sistem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioSistemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioSistemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flcode/inventario_sistem/InventarioSistemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flcode.inventario_sistem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioSistemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
